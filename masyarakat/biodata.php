4389fa8ec0fc00c3c6cbd98ab0196a539f07c41d
Nia Melinda <127264656+Niamelin06@users.noreply.github.com>
2023-03-08 07:50:58 +0700

    Add files via upload

---
+++ masyarakat/biodata.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Biodata nia</title>
+</head>
+<body style="background: url(../img/bg.JPG); background-size: cover;">
+<div class="row">
+<div>
+                      <img class="box" src="../img/cv.JPG">
+</div>
+</body>
+</html>
